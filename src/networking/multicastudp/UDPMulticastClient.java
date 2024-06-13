3dfcb4952951204fa3cc6ed5274523bce749cb11
22FI024HaruyaOno <22fi024@ms.dendai.ac.jp>
2024-06-13 14:50:26 +0900

    finish

--- src/networking/multicastudp/UDPMulticastClient.java
+++ src/networking/multicastudp/UDPMulticastClient.java
@@ -3,28 +3,27 @@ package networking.multicastudp;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
-import java.util.Scanner;
 
 public class UDPMulticastClient {
     public static void main(String[] argv)
             throws Exception {
         // UDPパケットを送信する先となるブロードキャストアドレス (5100番ポート) //ネットワーク部が全てビットがたっているものが
-        // ブロードキャストアドレス
+        //ブロードキャストアドレス
         /*
-         * 192.168.1.255」は自分のネットワーク環境に合わせて書き変える必要がある。
-         * ホストが所属しているネットワークが192.168.0.xxx/255.255.255.0なら「192.168.0.255」と指定する。
+        192.168.1.255」は自分のネットワーク環境に合わせて書き変える必要がある。
+        ホストが所属しているネットワークが192.168.0.xxx/255.255.255.0なら「192.168.0.255」と指定する。
          */
-        InetSocketAddress remoteAddress = new InetSocketAddress("133.14.222.123", 2591);
+        InetSocketAddress remoteAddress =
+                new InetSocketAddress("133.14.222.123", 2591);
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("メッセージを入力してください ↓");
-        String str = scanner.nextLine();
 
+        String str= "HELLO";
         // UDPパケットに含めるデータ
         byte[] sendBuffer = str.getBytes();
 
         // UDPパケット
-        DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, remoteAddress);
+        DatagramPacket sendPacket =
+                new DatagramPacket(sendBuffer, sendBuffer.length, remoteAddress);
 
         // DatagramSocketインスタンスを生成して、UDPパケットを送信
         new DatagramSocket().send(sendPacket);
--- src/networking/multicastudp/UDPMulticastServer.java
+++ src/networking/multicastudp/UDPMulticastServer.java
@@ -4,20 +4,24 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 public class UDPMulticastServer {
-    public static void main(String[] argv) throws Exception {
-        // 2591番ポートを監視するUDPソケットを生成
+    public static void main(String[] argv)
+            throws Exception {
+        // 5100番ポートを監視するUDPソケットを生成
         DatagramSocket receiveSocket = new DatagramSocket(2591);
 
         // 受け付けるデータバッファとUDPパケットを作成
         byte receiveBuffer[] = new byte[1024];
-        DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+        DatagramPacket receivePacket =
+                new DatagramPacket(receiveBuffer, receiveBuffer.length);
 
         while (true) {
             // UDPパケットを受信
             receiveSocket.receive(receivePacket);
 
             // 受信したデータを標準出力へ出力
-            System.out.println( new String( receivePacket.getData(), 0, receivePacket.getLength() ) );
+            System.out.println
+                    (new String(receivePacket.getData(),
+                            0, receivePacket.getLength()));
         }
     }
 }
